8cac9c3ecc939eebc6e7e56dd54f34baea6eb1f7
shun-inoue138 <ht8sk13@icloud.com>
2020-05-07 23:32:29 +0900

    【CMS（商品閲覧、編集、登録、削除）機能】Categoryモデルの作成、Productモデルとともに＄fillableの設定。リレーションの設定。

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class product extends Model
 {
-    //
+    protected $fillable=[
+        'name','description','price','img_path','category_id','stock'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo('\App\Category');
+    }
 }
