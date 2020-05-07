9d7ad7c212b35db38059a9aed3a95494353eb3c9
shun-inoue138 <ht8sk13@icloud.com>
2020-05-07 23:26:10 +0900

    【CMS（商品閲覧、編集、登録、削除）機能】ルート定義。

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,11 @@ Auth::routes();
 Route::post('/cart', 'CartsController@in')->name('in');
 Route::get('/cart', 'CartsController@index')->name('cart');
 Route::delete('/cart', 'CartsController@decrease')->name('decrease');
-Route::get('/', 'ProductsController@index')->name('home');
-Route::get('/{id}', 'productsController@detail')->name('detail');
+Route::get('/', 'ProductsController@index')->name('product_list');
+Route::get('/{id}', 'ProductsController@detail')->name('detail');
+Route::get('/management', 'ManagementsController@index')->name('management_top');
+Route::get('/management/{id}', 'ManagementsController@detail')->name('management_detail');
+Route::put('/management/{id}/edit', 'ManagementsController@edit')->name('edit');
+Route::delete('/management/{id}/delete', 'ManagementsController@delete')->name('delete');
+Route::post('/management/new', 'ManagementsController@new')->name('new');
 
