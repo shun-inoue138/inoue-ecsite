0ebba680f68657ccc37da3501a4641bf6d32cc1a
shun-inoue138 <ht8sk13@icloud.com>
2020-05-12 07:13:54 +0900

    存在しないフィールド、item_idの削除。

--- app/Cart.php
+++ app/Cart.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     protected $fillable = [
-        'product_id','user_id', 'item_id', 'quantity'
+        'product_id','user_id', 'quantity'
     ];
     public function product()
     {
